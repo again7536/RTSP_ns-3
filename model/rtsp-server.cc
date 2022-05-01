be08b3d7d72ecd6b61302a8f41515a0bf8b5990a
again7536 <45936642+again7536@users.noreply.github.com>
2022-05-01 12:15:34 +0000

    [Edit] initial structure

--- model/rtsp-server.cc
+++ model/rtsp-server.cc
@@ -1,1 +1,186 @@
-//rtsp-server code
+#include "rtsp-server.h"
+
+#include <ns3/log.h>
+#include <ns3/simulator.h>
+#include <ns3/callback.h>
+#include <ns3/config.h>
+#include <ns3/pointer.h>
+#include <ns3/uinteger.h>
+#include <ns3/three-gpp-http-variables.h>
+#include <ns3/packet.h>
+#include <ns3/socket.h>
+#include <ns3/tcp-socket.h>
+#include <ns3/tcp-socket-factory.h>
+#include <ns3/inet-socket-address.h>
+#include <ns3/inet6-socket-address.h>
+#include <ns3/unused.h>
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE("RtspServer");
+
+NS_OBJECT_ENSURE_REGISTERED(RtspServer);
+
+TypeId
+RtspServer::GetTypeId (void) 
+{
+    static TypeId tid = TypeId("ns3::RtspServer")
+        .SetParent<Application> ()
+        .SetGroupName("Applications")
+        .AddConstructor<RtspServer>()
+        .AddAttribute ("LocalAddress",
+                    "The local address of the server, "
+                    "i.e., the address on which to bind the Rx socket.",
+                    AddressValue (),
+                    MakeAddressAccessor (&ThreeGppHttpServer::m_localAddress),
+                    MakeAddressChecker ())
+        .AddAttribute ("LocalPort",
+                    "Port on which the application listen for incoming packets.",
+                    UintegerValue (80), // the default HTTP port
+                    MakeUintegerAccessor (&ThreeGppHttpServer::m_localPort),
+                    MakeUintegerChecker<uint16_t> ())
+    ;
+    return tid;
+}
+
+RtspServer::RtspServer () : m_lossCounter (0) 
+{
+    NS_LOG_FUNCTION (this);
+    
+    m_sendDelay = FRAME_PERIOD;
+    m_received = 0;
+
+    buf = new char[20000];
+}
+
+void
+RtspServer::DoDispose ()
+{
+  NS_LOG_FUNCTION (this);
+
+  if (!Simulator::IsFinished ())
+    {
+      StopApplication ();
+    }
+
+  Application::DoDispose (); // Chain up.
+}
+
+void
+RtspServer::StartApplication ()
+{
+    NS_LOG_FUNCTION (this);
+
+    if (m_state == INIT)
+    {
+        if (m_initialSocket == 0)
+        {
+            // Find the current default MTU value of TCP sockets.
+            Ptr<const ns3::AttributeValue> previousSocketMtu;
+            const TypeId tcpSocketTid = TcpSocket::GetTypeId ();
+            for (uint32_t i = 0; i < tcpSocketTid.GetAttributeN (); i++)
+            {
+                struct TypeId::AttributeInformation attrInfo = tcpSocketTid.GetAttribute (i);
+                if (attrInfo.name == "SegmentSize")
+                {
+                    previousSocketMtu = attrInfo.initialValue;
+                }
+            }
+
+            // Creating a TCP socket to connect to the server.
+            m_initialSocket = Socket::CreateSocket (GetNode (),
+                                                    TcpSocketFactory::GetTypeId ());
+            m_initialSocket->SetAttribute ("SegmentSize", UintegerValue (m_mtuSize));
+
+            int ret;
+
+            if (Ipv4Address::IsMatchingType (m_localAddress))
+            {
+                const Ipv4Address ipv4 = Ipv4Address::ConvertFrom (m_localAddress);
+                const InetSocketAddress inetSocket = InetSocketAddress (ipv4,
+                                                                        m_rtspPort);
+                NS_LOG_INFO (this << " Binding on " << ipv4
+                                    << " port " << m_rtspPort
+                                    << " / " << inetSocket << ".");
+                ret = m_initialSocket->Bind (inetSocket);
+                NS_LOG_DEBUG (this << " Bind() return value= " << ret
+                                    << " GetErrNo= "
+                                    << m_initialSocket->GetErrno () << ".");
+            }
+            else if (Ipv6Address::IsMatchingType (m_localAddress))
+            {
+                const Ipv6Address ipv6 = Ipv6Address::ConvertFrom (m_localAddress);
+                const Inet6SocketAddress inet6Socket = Inet6SocketAddress (ipv6,
+                                                                            m_rtspPort);
+                NS_LOG_INFO (this << " Binding on " << ipv6
+                                    << " port " << m_localPort
+                                    << " / " << inet6Socket << ".");
+                ret = m_initialSocket->Bind (inet6Socket);
+                NS_LOG_DEBUG (this << " Bind() return value= " << ret
+                                    << " GetErrNo= "
+                                    << m_initialSocket->GetErrno () << ".");
+            }
+
+            ret = m_initialSocket->Listen ();
+            NS_LOG_DEBUG (this << " Listen () return value= " << ret
+                                << " GetErrNo= " << m_initialSocket->GetErrno ()
+                                << ".");
+
+            NS_UNUSED (ret);
+
+        } // end of `if (m_initialSocket == 0)`
+
+        NS_ASSERT_MSG (m_initialSocket != 0, "Failed creating socket.");
+
+        // 여기서부터 소켓 콜백 작성 필요
+        // 아래는 http 예시
+
+        // m_initialSocket->SetAcceptCallback (MakeCallback (&ThreeGppHttpServer::ConnectionRequestCallback,
+        //                                                     this),
+        //                                     MakeCallback (&ThreeGppHttpServer::NewConnectionCreatedCallback,
+        //                                                     this));
+        // m_initialSocket->SetCloseCallbacks (MakeCallback (&ThreeGppHttpServer::NormalCloseCallback,
+        //                                                     this),
+        //                                     MakeCallback (&ThreeGppHttpServer::ErrorCloseCallback,
+        //                                                     this));
+        // m_initialSocket->SetRecvCallback (MakeCallback (&ThreeGppHttpServer::ReceivedDataCallback,
+        //                                                 this));
+        // m_initialSocket->SetSendCallback (MakeCallback (&ThreeGppHttpServer::SendCallback,
+        //                                                 this));
+        m_state = READY;
+
+    } // end of `if (m_state == NOT_STARTED)`
+    else
+    {
+      NS_FATAL_ERROR ("Invalid state " << GetStateString ()
+                                       << " for StartApplication().");
+    }
+
+} // end of `void StartApplication ()`
+
+void
+RtspServer::StopApplication ()
+{
+  NS_LOG_FUNCTION (this);
+
+  m_state = INIT;
+
+  // Close all accepted sockets.
+  // m_txBuffer->CloseAllSockets ();
+
+  // Stop listening.
+  if (m_initialSocket != 0)
+    {
+      m_initialSocket->Close ();
+      m_initialSocket->SetAcceptCallback (MakeNullCallback<bool, Ptr<Socket>, const Address &> (),
+                                          MakeNullCallback<void, Ptr<Socket>, const Address &> ());
+      m_initialSocket->SetCloseCallbacks (MakeNullCallback<void, Ptr<Socket> > (),
+                                          MakeNullCallback<void, Ptr<Socket> > ());
+      m_initialSocket->SetRecvCallback (MakeNullCallback<void, Ptr<Socket> > ());
+      m_initialSocket->SetSendCallback (MakeNullCallback<void, Ptr<Socket>, uint32_t > ());
+    }
+}
+
+/* 자유롭게 추가 */
+
+}
--- model/rtsp-server.h
+++ model/rtsp-server.h
@@ -1,12 +1,25 @@
-//rtsp-server header
+/*
+
+three-gpp-http-server.h랑 server.java 참고해서 만들었습니다.
+각자 해야하는 역할 (노션에 적은 것) 주석 달아놨으니 서치해서 
+함수/클래스 구현하면 될 것 같습니다.
+변수랑 메소드 전부 적어 놓은게 아니라서 필요하면 추가해서 쓰면 됩니다!!
+
+*/
 
 #ifndef RTSP_SERVER_H
 #define RTSP_SERVER_H
 
-#include "ns3/application.h"
-#include "ns3/event-id.h"
-#include "ns3/ptr.h"
-#include "ns3/ipv4-address.h"
+#include <ns3/ptr.h>
+#include <ns3/simple-ref-count.h>
+#include <ns3/nstime.h>
+#include <ns3/event-id.h>
+#include <ns3/three-gpp-http-header.h>
+#include <ns3/application.h>
+#include <ns3/address.h>
+#include <ns3/traced-callback.h>
+#include <map>
+#include <ostream>
 
 namespace ns3 {
 
@@ -14,29 +27,11 @@ class RtspServer : public Application
 {
 public:
     static TypeId GetTypeId (void);
-    
     RtspServer();
-
     ~RtspServer();
-    
 
-private:
-    Address m_peerAddress;   //Client IP address
-    int m_rtp_port = 0;      //destination port for RTP packets  (given by the RTSP Client)
-    int m_rtsp_dest_port = 0;
-
-    int m_imagenb = 0;   //image nb of the image currently transmitted
-    //VideoStream video; //VideoStream object used to access video frames
-
-    //Timer timer;    //timer used to send the images at the video frame rate
-    //byte[] buf;     //buffer used to store the images to send to the client 
-    int m_sendDelay;  //the delay to send images over the wire. Ideally should be
-                      //equal to the frame rate of the video file, but may be 
-                      //adjusted when congestion is detected.
-
-    //RTSP variables
-    //----------------
-    const static enum = {
+    enum State_t
+    {
         //rtsp states
         INIT,
         READY,
@@ -48,8 +43,49 @@ private:
         TEARDOWN,
         DESCRIBE
     };
+
+private:
+    /**************************************************
+    *                   소켓 콜백
+    **************************************************/
+    //Parse RTSP Request
+    void ReceivedDataCallback (Ptr<Socket> socket);
+    //Send RTSP Response
+    void SendCallback (Ptr<Socket> socket, uint32_t availableBufferSize);
+
+    /**************************************************
+    *                    메소드
+    ***************************************************/
+    void DoDispose();
+    void StartApplication();
+    void StopApplication();
+    
+    //Handler for timer
+    void ActionPerformed();
+
+    /**************************************************
+    *                      변수
+    ***************************************************/
+    Ptr<Socket> m_initialSocket;
+    Address m_localAddress;          //Server IP address
+    Address m_clientAddress;         //Client IP address
+    uint16_t m_rtpPort = 0;          //destination port for RTP packets  (given by the RTSP Client)
+    uint16_t m_rtspPort = 0;
+    uint32_t m_mtuSize = 0;
+
+    int m_imagenb = 0;               //image nb of the image currently transmitted
+    //VideoStream video;             //VideoStream object used to access video frames
+
+    //Timer timer;                   //timer used to send the images at the video frame rate
+    //byte[] buf;                    //buffer used to store the images to send to the client 
+    int m_sendDelay;                 //the delay to send images over the wire. Ideally should be
+                                     //equal to the frame rate of the video file, but may be 
+                                     //adjusted when congestion is detected.
+
+    //RTSP variables
+    //----------------
     int m_seqNum;
-    int m_state = 0;
+    State_t m_state = 0;
     
     const static int FRAME_PERIOD = 100; //Frame period of the video to stream, in ms
     const static int MJPEG_TYPE = 26;    //RTP payload type for MJPEG video
@@ -64,4 +100,31 @@ private:
     const static String CRLF = "\r\n";
 }
 
+
+//Controls RTP sending rate based on traffic
+class CongestionController{
 }
+
+//Listeners for RTCP packets sent from client
+class RtcpReceiver {
+private:
+    // private Timer rtcpTimer;
+    // private byte[] rtcpBuf;
+    // int interval;
+}
+
+//Translate an image to different encoding or quality
+class ImageTranslator {
+private:
+        // float compressionQuality;
+        // ByteArrayOutputStream baos;
+        // BufferedImage image;
+        // Iterator<ImageWriter>writers;
+        // ImageWriter writer;
+        // ImageWriteParam param;
+        // ImageOutputStream ios;
+}
+
+}
+
+#endif
